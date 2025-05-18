ca749569823ba650f0971318e3acf9333e652f58
Maksims Radugins <maxim@radugin.com>
2025-05-18 11:17:23 +0300

    Target macOS v14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Remote Input",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v14)
     ],
     products: [
         .executable(
