8582f94d216bae8dcf1c13718d0c11c43c8f976b
Maksims Radugins <maxim@radugin.com>
2025-05-18 22:07:03 +0300

    Add Xcode project

---
+++ RemoteInputTests/RemoteInputTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteInputTests.swift
+//  RemoteInputTests
+//
+//  Created by Maksims Radugins on 18/05/2025.
+//
+
+import Testing
+
+struct RemoteInputTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
